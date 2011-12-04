7672da21381269991873df81f54b5f7746c69aef
Gustav Ernberg <gustav@purepro.se>
2011-12-04 22:23:38 +0100

    Bättre namn...

--- se/ernberg/TestWindow.java
+++ se/ernberg/TestWindow.java
@@ -16,7 +16,7 @@ public class TestWindow {
 
 	private static void createAndShowGUI() {
 		// Create and set up the window.
-		JFrame frame = new JFrame("HelloWorldSwing");
+		JFrame frame = new JFrame("Tvåkomponentstestare");
 		final Container panel = frame.getContentPane();
 		panel.setLayout(new FlowLayout());// new BoxLayout(panel,
 											// BoxLayout.Y_AXIS));
